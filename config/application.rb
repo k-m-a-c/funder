a3f81ab0397438d4784c5453f98b41e923f3f6db
Kevin McLoughlin <kevinmcloughlin2010@gmail.com>
2015-09-30 12:38:03 -0400

    add kaminari pagination styling

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,12 @@ module Funder
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.assets.configure do |env|
+      if Rails.env.development? || Rails.env.test?
+        env.cache = ActiveSupport::Cache.lookup_store(:memory_store)
+      end
+    end
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
